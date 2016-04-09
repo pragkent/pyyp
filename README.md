e47ce721874628b225ac97e848e7cdf75d927e64
Kent Wang <pragkent@gmail.com>
2016-04-09 22:39:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+pyyp
+====
+
+[![Build Status](https://travis-ci.org/pragkent/pyyp.png?branch=master)](https://travis-ci.org/pragkent/pyyp)
+
+pyyp is a wrapper of the Yunpian SMS API, written in Python.
+
+Installation
+------------
+
+Install pyyp with `pip`:
+    pip install pyyp
+
+Example
+-------
+```python
+import pyyp
+
+yunpian = pyyp.Yunpian(api_key=API_KEY)
+yunpian.send(mobile, 'hello yunpian!')
+
+```
+
+TODO
+----
+- Add unit tests
---
+++ tests/test_models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from pyyp import models
+
+class TestRequests:
+    def test_sms_send_request(self):
+        r = models.SMSSendRequest(
+                apikey='API_KEY', mobile='13812345678', text='hello')
+        assert r.uri == 'https://sms.yunpian.com/v2/sms/single_send.json'
+        assert r.params == {'apikey': 'API_KEY', 'mobile': '13812345678',
+                'text': 'hello'}
+
+    def test_sms_batch_send_request(self):
+        r = models.SMSBatchSendRequest(
+                apikey='API_KEY',
+                mobile='13812345678,13887654321',
+                text='hello')
+        assert r.uri == 'https://sms.yunpian.com/v2/sms/batch_send.json'
+        assert r.params == {
+                'apikey': 'API_KEY',
+                'mobile': '13812345678,13887654321',
+                'text': 'hello'
+               }
