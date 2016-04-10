82247c37f0e8b8b11ad941195bbe91f1e3605965
Kent Wang <pragkent@gmail.com>
2016-04-10 22:29:39 +0800

    Reorganize test codes.

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,22 +2,31 @@
 
 from pyyp import models
 
-class TestRequests:
-    def test_sms_send_request(self):
-        r = models.SMSSendRequest(
-                apikey='API_KEY', mobile='13812345678', text='hello')
-        assert r.url == 'https://sms.yunpian.com/v2/sms/single_send.json'
-        assert r.params == {'apikey': 'API_KEY', 'mobile': '13812345678',
-                'text': 'hello'}
 
-    def test_sms_batch_send_request(self):
-        r = models.SMSBatchSendRequest(
-                apikey='API_KEY',
-                mobile='13812345678,13887654321',
-                text='hello')
-        assert r.url == 'https://sms.yunpian.com/v2/sms/batch_send.json'
-        assert r.params == {
-                'apikey': 'API_KEY',
-                'mobile': '13812345678,13887654321',
-                'text': 'hello'
-               }
+def test_sms_send_request():
+    r = models.SMSSendRequest(
+            apikey='API_KEY', mobile='13812345678', text='hello')
+    params = {
+        'apikey': 'API_KEY',
+        'mobile': '13812345678',
+        'text': 'hello'
+    }
+
+    assert r.url == 'https://sms.yunpian.com/v2/sms/single_send.json'
+    assert r.params == params
+
+
+def test_sms_batch_send_request():
+    r = models.SMSBatchSendRequest(
+            apikey='API_KEY',
+            mobile='13812345678,13887654321',
+            text='hello')
+
+    params = {
+        'apikey': 'API_KEY',
+        'mobile': '13812345678,13887654321',
+        'text': 'hello'
+    }
+
+    assert r.url == 'https://sms.yunpian.com/v2/sms/batch_send.json'
+    assert r.params == params
