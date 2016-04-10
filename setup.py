20dbb7976501b3e8b555e5c7e7500ee6ef7b9829
Kent Wang <pragkent@gmail.com>
2016-04-10 10:43:58 +0800

    Update README and setup informations

--- setup.py
+++ setup.py
@@ -1,46 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+setup.py
+~~~~~~~~
 
-import os
-import sys
+This module provides the setup script for pyyp.
+"""
+
+from setuptools import setup
+from codecs import open
 
 import pyyp
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
-required = ['requests>=2.0.0']
-
+with open('README.rst', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(
     name='pyyp',
+
     version=pyyp.__version__,
+
     description='Yunpian API wrapper.',
-    long_description=open('README.md').read(),
+    long_description=readme,
+
+    url='https://github.com/pragkent/pyyp',
+
     author='Kent Wang',
     author_email='pragkent@gmail.com',
-    url='https://github.com/pragkent/pyyp',
-    packages=['pyyp'],
-    package_data={'': ['LICENSE',]},
-    include_package_data=True,
-    install_requires=required,
+
     license='MIT',
-    classifiers=(
-        # 'Development Status :: 5 - Production/Stable',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
+
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-    ),
+    ],
+
+    packages=['pyyp'],
+
+    install_requires=['requests>=2.0.0'],
+
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
 )
